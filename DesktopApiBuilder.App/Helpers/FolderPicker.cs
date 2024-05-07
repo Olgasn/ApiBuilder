0abd4298a7cc58b81e5008174913d4a8ca0ccc7e
Vlada <70849026+Vlada25@users.noreply.github.com>
2024-05-07 20:08:08 +0300

    add folder picker (#8)
    Co-authored-by: Vlada Leonenko <uladzislava.liavonenka@innowise.com>

---
+++ DesktopApiBuilder.App/Helpers/FolderPicker.cs
@@ -0,0 +1,22 @@
+﻿namespace DesktopApiBuilder.App.Helpers;
+
+public static class FolderPicker
+{
+    public static async Task<string> PickFolder(string? currentLocation)
+    {
+        var folderPicker = new Windows.Storage.Pickers.FolderPicker();
+        var hwnd = ((MauiWinUIWindow)App.Current!.Windows[0].Handler.PlatformView!).WindowHandle;
+
+        WinRT.Interop.InitializeWithWindow.Initialize(folderPicker, hwnd);
+
+        var folder = await folderPicker.PickSingleFolderAsync();
+        var path = currentLocation;
+
+        if (folder is not null)
+        {
+            path = folder.Path;
+        }
+
+        return path ?? string.Empty;
+    }
+}
--- DesktopApiBuilder.App/Services/SolutionService.cs
+++ DesktopApiBuilder.App/Services/SolutionService.cs
@@ -7,9 +7,9 @@ public static class SolutionService
     public static void CreateSolution(SolutionSettingsModel solutionSettings)
     {
         ProcessManager.ExecuteCmdCommands([
-            $"cd {solutionSettings.SolutionPath}",
+            $"cd /d {solutionSettings.SolutionPath}",
             $"mkdir {solutionSettings.SolutionName}",
-            $"cd {solutionSettings.FullSolutionPath}",
+            $"cd /d {solutionSettings.FullSolutionPath}",
             $"dotnet new sln --name {solutionSettings.SolutionName}"
         ]);
     }
