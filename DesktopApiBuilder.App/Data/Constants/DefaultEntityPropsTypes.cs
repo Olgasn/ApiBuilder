ded0821a0377f31d7a39f0012d4966874622f594
Vlada Leonenko <uladzislava.liavonenka@innowise.com>
2024-06-24 12:35:30 +0200

    Add DateTime support

--- DesktopApiBuilder.App/Data/Constants/DefaultEntityPropsTypes.cs
+++ DesktopApiBuilder.App/Data/Constants/DefaultEntityPropsTypes.cs
@@ -2,5 +2,13 @@
 
 public static class DefaultEntityPropsTypes
 {
-    public readonly static List<string> Types = ["string", "int", "double", "decimal", "bool", "Guid"];
+    public readonly static List<string> Types = [ 
+        "string", 
+        "int", 
+        "double", 
+        "decimal", 
+        "bool", 
+        "Guid", 
+        "DateTime" 
+    ];
 }
